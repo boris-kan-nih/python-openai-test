03094d7048a8e2fd2ca8d7f2bd2acd56550032a7
boris-kan-nih <boris.kan@nih.gov>
2023-10-27 15:06:07 -0400

    added an faq function to allow user input an url for simple faq

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ openai.api_key = os.getenv('API_KEY')
 def index():
    return render_template('index.html')
 
+@app.route('/inserturl', methods=['POST'])
+def inserturl():
+    url = request.form['url'] 
+    generated_faq = faq(url)
+    return render_template('faq.html', questions=generated_faq, url=url)
+
 @app.route('/upload', methods=['POST'])
 def upload():
     uploaded_file = request.files['file']
@@ -66,29 +72,58 @@ def upload():
             # question:
             question_elem = ET.SubElement(questiontext, "question")
             question_elem.text = f"<![CDATA[<p dir=\"ltr\" style=\"text-align: left;\">question {question['question'][0]}</p>]]>"
-            # everything between the first and the last item. There is a weird /n from the render that cause an extra item so we are skipping it.
 
-            answer_elem = ET.SubElement(question_elem, "answer", format="html")
-            answer_text_elem = ET.SubElement(answer_elem, "text")
-
-            # last item
+            #  extra item so we are skipping it.
             answer_clean_up = question['question'][-1][8:]
-            # answer_text_elem.text = f"<![CDATA[<p>{answer_clean_up}</p>]]>"
 
+            # everything between the first and the last item. There is a weird /n from the render that cause an extra item so we are skipping it.
             for item in question['question'][1:-2]:
-                answer_text_elem.text = f"<![CDATA[<p>{item}</p>]]>"
                 if answer_clean_up == item:
+                    answer_elem = ET.SubElement(question_elem, "answer", format="html")
                     answer_elem.set("fraction", "100")
+                    answer_text_elem = ET.SubElement(answer_elem, "text")
+                    answer_text_elem.text = f"<![CDATA[<p>{item}</p>]]>"
                 else:
+                    answer_elem = ET.SubElement(question_elem, "answer", format="html")
                     answer_elem.set("fraction", "0")
+                    answer_text_elem = ET.SubElement(answer_elem, "text")
+                    answer_text_elem.text = f"<![CDATA[<p>{item}</p>]]>"
 
         response = ET.tostring(root).decode('utf-8')
         return Response(response, content_type='text/xml')
     return Response('<error>No file selected!</error>', content_type='text/xml')    
 
+def faq(url):
+    # Use OpenAI API to generate a faq
+    prompt = f"Create a frequenting asked question based on the following url: {url}. Provide the correct answer at the end."
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # You can use other engines like "text-davinci-002" based on your needs
+        prompt=prompt,
+        max_tokens=200,  # You can adjust the max tokens based on the desired length of the response
+        n=4,  # Number of questions you want to generate
+        stop=None
+    )
+    print(response)
+    # Parse OpenAI response and construct multiple-choice questions
+    questions = []
+    for choice in response.choices:
+        question_text = choice['text'].strip().splitlines()
+        
+        # Construct the question dictionary
+        question_dict = {
+            "question": question_text,
+        }
+        
+        questions.append(question_dict)
+    print(questions)
+    return questions
+
 def generate_questions(text):
     # Use OpenAI API to generate multiple-choice questions based on the extracted text
-    prompt = f"Create multiple-choice questions based on the following text: {text}\n\nAnswer each question with the most appropriate choice: A, B, C, or D.\n\nProvide the correct answer at the end."
+    # https://www.niaid.nih.gov/sites/default/files/score-investigator-responsibilities.pdf
+    # prompt = f"Create multiple-choice questions based on the following text: {text}\n\nAnswer each question with the most appropriate choice: A, B, C, or D in a new line.\n\nProvide the correct answer at the end."
+    # prompt = f"Create multiple-choice questions based on the following pdf: https://www.niaid.nih.gov/sites/default/files/score-investigator-responsibilities.pdf\n\nAnswer each question with the most appropriate choice: A, B, C, or D in a new line.\n\nProvide the correct answer at the end."
+    prompt = f"Create 6 frequenting asked questions based on the following pdf: https://www.niaid.nih.gov/sites/default/files/score-investigator-responsibilities.pdf\n\nProvide the correct answer at the end."
     response = openai.Completion.create(
         engine="text-davinci-003",  # You can use other engines like "text-davinci-002" based on your needs
         prompt=prompt,
@@ -99,18 +134,12 @@ def generate_questions(text):
     
     # Parse OpenAI response and construct multiple-choice questions
     questions = []
-    print
     for choice in response.choices:
         question_text = choice['text'].strip().splitlines()
-        # For simplicity, let's create options A, B, C, D as the answers
-        # options = ["A", "B", "C", "D"]
-        # correct_answer = options.pop()
         
         # Construct the question dictionary
         question_dict = {
             "question": question_text,
-            # "options": options,
-            # "correct_answer": correct_answer
         }
         
         questions.append(question_dict)
